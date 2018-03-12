1674353c26dfe8f0aa468d1b15d2d9e70b74e25e
Alex Lee <al8876@gmail.com>
2018-03-12 13:23:01 -0700

    test

--- frontend/App.js
+++ frontend/App.js
@@ -13,7 +13,6 @@ import {
   WebView,
 } from 'react-native';
 import Exponent, { Constants, ImagePicker, registerRootComponent, LinearGradient } from 'expo';
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -30,15 +29,12 @@ export default class App extends React.Component {
   setScreen(screen) {
     this.setState({screen: screen})
   };
-
   setUploading(bool) {
     this.setState({uploading: bool})
   };
-
   setImage(img) {
     this.setState({image: img})
   };
-
   // ---------------------
   // render state: should only have components - TODO: Dry up code and take out logic
   // ---------------------
@@ -51,13 +47,11 @@ export default class App extends React.Component {
     if (this.state.screen === 'HOME') {
       // return (<HomeScreen setScreen={this.setScreen.bind(this)} nextScreen={this.state.screen}/>)
       return (<HomeScreen {...this.state} setScreen={this.setScreen.bind(this)} setUploading={this.setUploading.bind(this)} setImage={this.setImage.bind(this)}/>)
-
     // ------------------------------------------------------
     // STEP 2: ANALYZE SCREEN - Take picture using native camera
     // ------------------------------------------------------
     } else if (this.state.screen === 'ANALYZE') {
       return (<Analyze {...this.state} setScreen={this.setScreen.bind(this)}/>)
-
     // ------------------------------------------------------
     // STEP 3: SPOTIFY SCREEN - After picture was taken
     // ------------------------------------------------------
@@ -66,8 +60,6 @@ export default class App extends React.Component {
     }
   }
 }
-
-
 // ------------------------------------------------------
 // HomeScreen Component: STEP 1
 // ------------------------------------------------------
@@ -76,7 +68,6 @@ class HomeScreen extends React.Component {
     super(props);
     this.state = {screen: this.props.screen};
   }
-
   _takePhoto = async () => {
     let pickerResult = await ImagePicker.launchCameraAsync({
       allowsEditing: false,
@@ -85,16 +76,17 @@ class HomeScreen extends React.Component {
     this.props.setScreen('ANALYZE');
     this._handleImagePicked(pickerResult);
   };
-
   _handleImagePicked = async pickerResult => {
     let uploadResponse, uploadResult;
     try {
+      // this.setState({ uploading: true });
       this.props.setUploading(true);
       if (!pickerResult.cancelled) {
         console.log(1);
         uploadResponse = await this.uploadImageAsync(pickerResult.uri);
         console.log(uploadResponse);
         uploadResult = await uploadResponse.json();
+        // this.setState({ image: uploadResult.location });
         this.props.setImage(uploadResult.location);
       }
     } catch (e) {
@@ -107,7 +99,6 @@ class HomeScreen extends React.Component {
       this.props.setUploading(false); 
     }
   };
-
   async uploadImageAsync(uri) {
     let apiUrl = 'https://file-upload-example-backend-dkhqoilqqn.now.sh/upload';
   
@@ -132,12 +123,10 @@ class HomeScreen extends React.Component {
   
     return fetch(apiUrl, options);
   }
-
   // ------------------------------------------------------
   // Called after the component was rendered and it was attached to the DOM.
   // This is a good place to make AJAX requests or setTimeout.
   // ------------------------------------------------------
-
   render() {
     return( 
     <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -152,13 +141,14 @@ class HomeScreen extends React.Component {
     );  
   }
 }
-
-
+// TODO: Create components and put methods in accordingly
+// ------------------------------------------------------
+// Analyze Screen Component: STEP 2
+// ------------------------------------------------------
 class Analyze extends React.Component {
   constructor(props) {
     super(props);
   }
-
   _maybeRenderImage = () => {
     let { image } = this.props;
     if (!image) {
@@ -182,10 +172,8 @@ class Analyze extends React.Component {
           }}>
           <Image source={{ uri: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/35771931234507.564a1d2403b3a.gif' }} style={{ width: 150, height: 150, borderRadius: 100 }} />
         </View>
-
         <Text style={{ color: 'white', fontSize: 20, paddingVertical: 30, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>  ANALYZING MOOD</Text>
       </View>
-
       )
     }
     // IF HAS IMAGE
@@ -213,12 +201,10 @@ class Analyze extends React.Component {
       </View>
     );
   };
-
   _returnState = () => {
     console.log('THIS IS THE STATE');
     console.log(this.props);
   }
-
   // ------------------------------------------------------
   // Called after the component was rendered and it was attached to the DOM.
   // This is a good place to make AJAX requests or setTimeout.
@@ -228,7 +214,6 @@ class Analyze extends React.Component {
       this.props.setScreen('PLAYLIST');
     }, 15000);
   }
-
   render() {
     return( 
       <View style={styles.container}>
@@ -240,8 +225,6 @@ class Analyze extends React.Component {
     );  
   }
 }
-
-
 // ------------------------------------------------------
 // HomeScreen Component: STEP 1
 // ------------------------------------------------------
@@ -250,26 +233,23 @@ class Playlist extends React.Component {
     super(props);
     this.state = {screen: this.props.screen};
   }
-
   _returnHome = () => {
     this.props.setScreen('HOME');
   };
-
   render() {
     return( 
       <View style={styles.container}>
         <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
-        <Text style={{ marginTop: 30, color: 'white', fontSize: 20, padding: 10 }}>YOUR CUSTOM PLAYLIST</Text>
+        <Text style={{ marginTop: 30, color: 'white' }}>YOUR CUSTOM PLAYLIST</Text>
         <Text style={{ color: 'white', paddingHorizontal: 30 }}>Our Artificial Inteligence shows you are 80% Happy and 20% calm today</Text>
         <WebView source={{uri: 'https://open.spotify.com/embed?uri=spotify%3Aalbum%3A2rp5riHULWgrXPsDtsp1ir'}} style={{ marginTop: 20, marginBottom: 30, height:380, width: 300 }}/>
         <TouchableOpacity onPress={this._returnHome}>
-          <Text style={{ fontSize: 20, color: 'white', padding: 20, paddingTop: 5 }}>START OVER</Text>
+          <Text style={{ fontSize: 20, color: 'white', paddingBottom: 10 }}>START OVER</Text>
         </TouchableOpacity>
       </View>
     );  
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
