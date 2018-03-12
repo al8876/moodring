36c27ece100256e6a7572b3ceba9eeb835e253b3
Alex Lee <al8876@gmail.com>
2018-03-12 15:47:57 -0700

    Radar junk

--- frontend/App.js
+++ frontend/App.js
@@ -13,6 +13,7 @@ import {
   WebView,
 } from 'react-native';
 import Exponent, { Constants, ImagePicker, registerRootComponent, LinearGradient } from 'expo';
+import Line from 'react-chartjs';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -138,19 +139,105 @@ class HomeScreen extends React.Component {
   // This is a good place to make AJAX requests or setTimeout.
   // ------------------------------------------------------
 
+  // render() {
+  //   return( 
+  //   <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  //     <View style={styles.container}>
+  //       <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
+  //       <TouchableOpacity onPress={this._takePhoto}>
+  //           <Text style={{ fontSize: 20, color: 'white' }}>TAP TO BEGIN</Text>
+  //           <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://78.media.tumblr.com/48a0d13c52b402e976bc5d4416552671/tumblr_onew3c4x8a1vxu8n6o1_500.gif' }} />
+  //           <Text>TEST</Text>
+  //       </TouchableOpacity>
+  //     </View>
+  //   </View >
+  //   );  
+  // }
+
   render() {
-    return( 
-    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={styles.container}>
-        <LinearGradient colors={['#5161B9', '#9C69CC']} style={{ position: 'absolute', height: 900, width: 400 }} />
-        <TouchableOpacity onPress={this._takePhoto}>
-            <Text style={{ fontSize: 20, color: 'white' }}>TAP TO BEGIN</Text>
-            <Image style={{ width: 150, height: 150 }} source={{ uri: 'https://78.media.tumblr.com/48a0d13c52b402e976bc5d4416552671/tumblr_onew3c4x8a1vxu8n6o1_500.gif' }} />
-        </TouchableOpacity>
+    var LineChart = require("react-chartjs").Line;
+    var chartData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
+    var chartOptions = {
+
+      ///Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines : true,
+    
+      //String - Colour of the grid lines
+      scaleGridLineColor : "rgba(0,0,0,.05)",
+    
+      //Number - Width of the grid lines
+      scaleGridLineWidth : 1,
+    
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+    
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines: true,
+    
+      //Boolean - Whether the line is curved between points
+      bezierCurve : true,
+    
+      //Number - Tension of the bezier curve between points
+      bezierCurveTension : 0.4,
+    
+      //Boolean - Whether to show a dot for each point
+      pointDot : true,
+    
+      //Number - Radius of each point dot in pixels
+      pointDotRadius : 4,
+    
+      //Number - Pixel width of point dot stroke
+      pointDotStrokeWidth : 1,
+    
+      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+      pointHitDetectionRadius : 20,
+    
+      //Boolean - Whether to show a stroke for datasets
+      datasetStroke : true,
+    
+      //Number - Pixel width of dataset stroke
+      datasetStrokeWidth : 2,
+    
+      //Boolean - Whether to fill the dataset with a colour
+      datasetFill : true,
+      //String - A legend template
+      legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>",
+    
+      //Boolean - Whether to horizontally center the label and point dot inside the grid
+      offsetGridLines : false,
+    };
+    return (
+      <View>
+        <Line data={chartData} options={chartOptions} width="600" height="250"/>
       </View>
-    </View >
-    );  
+    )
   }
+
+
 }
 
 
@@ -209,7 +296,7 @@ class Analyze extends React.Component {
           }}>
           <Image source={{ uri: image }} style={{ width: 150, height: 150, borderRadius: 100 }} />
         </View>
-        <Text style={{ color: 'white', fontSize: 20, paddingVertical: 30, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>  ANALYZING MOOD</Text>
+          <Text style={{ color: 'white', fontSize: 20, paddingVertical: 30, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>  ANALYZING MOOD</Text>
       </View>
     );
   };
@@ -236,6 +323,7 @@ class Analyze extends React.Component {
           <TouchableOpacity onPress={this._returnState()}>
             {this._maybeRenderImage()}
           </TouchableOpacity>
+          <Image source={{ uri: 'https://www.r-graph-gallery.com/wp-content/uploads/2016/05/142_basic_radar_chart_2.png' }} style={{ width: 150, height: 150, borderRadius: 100 }}/>          
       </View>
     );  
   }
